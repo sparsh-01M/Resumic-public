f1ad0010dd05309f0c4d46d16792f5118a2c4880
sparsh-01M <singhalsparshu9876@gmail.com>
2025-06-23 16:34:46 +0530

    Update JobsPage.tsx

--- frontend/src/pages/JobsPage.tsx
+++ frontend/src/pages/JobsPage.tsx
@@ -533,13 +533,13 @@ const JobsPage = () => {
                 Discover your next career move with our curated job listings
               </p>
             </div>
-            <Button
+            {/* <Button
               onClick={openCreateJobModal}
               className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3"
             >
               <Plus className="w-5 h-5 mr-2" />
               Create Job
-            </Button>
+            </Button> */}
           </div>
         </div>
 
